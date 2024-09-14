b7224a754e35d0806c0305ea7a4498efc2ac52e7
anepretimovdiasoft <94528503+anepretimovdiasoft@users.noreply.github.com>
2024-09-14 07:52:12 +0300

    Часть 2
    Добавление обработки нажатий, хитбокса у мухи и сообщении о победе

--- core/src/main/java/ru/quickstartschool/Main.java
+++ core/src/main/java/ru/quickstartschool/Main.java
@@ -5,26 +5,44 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Main extends ApplicationAdapter {
 
     Texture backGround;
+
     Texture flyTexture;
+    Rectangle flyHitBox;
+    boolean isAlive;
 
     SpriteBatch batch;
     OrthographicCamera camera;
+
+    Vector3 touchPos;
+
+    BitmapFont font;
 
     @Override
     public void create() {
 
         backGround = new Texture(Gdx.files.internal("background.png"));
+        font = new BitmapFont();
+        font.getData().setScale(2);
+        font.setColor(Color.BLACK);
+
         flyTexture = new Texture(Gdx.files.internal("fly.png"));
+        flyHitBox = new Rectangle(800 / 2, 480 / 2, 64, 64);
+        isAlive = true;
 
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 800, 480);
         batch = new SpriteBatch();
+
+        touchPos = new Vector3();
     }
 
     @Override
@@ -35,11 +53,22 @@ public class Main extends ApplicationAdapter {
         batch.setProjectionMatrix(camera.combined);
 
         batch.begin();
-
         batch.draw(backGround, 0, 0, 800, 480);
-        batch.draw(flyTexture, 800 / 2, 480 / 2, 64, 64);
-
+        if (isAlive) {
+            batch.draw(flyTexture, 800 / 2, 480 / 2, 64, 64);
+        } else {
+            font.draw(batch, "WIN!", 800 / 2, 480 / 2);
+        }
         batch.end();
+
+        if (Gdx.input.isTouched()) {
+            touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+            camera.unproject(touchPos);
+            if (flyHitBox.contains(touchPos.x, touchPos.y)) {
+                isAlive = false;
+            }
+        }
+
     }
 
     @Override
@@ -47,5 +76,6 @@ public class Main extends ApplicationAdapter {
         backGround.dispose();
         flyTexture.dispose();
         batch.dispose();
+        font.dispose();
     }
 }
